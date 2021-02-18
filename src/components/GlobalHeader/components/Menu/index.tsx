cc1ba92ea1243a0b9d753d56e849e753550f1337
张凯荣 <zhangkr@dehuinet.com>
2021-02-18 13:56:07 +0800

    feat  globalHeader导航完成 globalHeader 完成

---
+++ src/components/GlobalHeader/components/Menu/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './Menu.scss'
+import {connect} from 'react-redux'
+import { Menu } from 'antd';
+import { Link } from 'react-router-dom'
+import {SvgIcons} from '@/components'
+import {constantsRouter} from '@/router/constants'
+
+export type MenuProps = {
+  setEntryKeyOptions: (key:string) => void;
+  entryKey: string
+}
+
+const EntryItem:React.FC<MenuProps> = (props) => {
+    //* 入口的key 
+  const entranceKey = ['平台管理','系统管理']
+    //* 入口的内容 -- 差一步⚠️
+  const entranceItem = constantsRouter.filter(item => entranceKey.includes(item.title))
+
+  const {entryKey,setEntryKeyOptions} = props
+  
+  const handleClick = (e:any) => {
+    setEntryKeyOptions(e.key)
+  };
+
+  return (
+    <Menu onClick={handleClick} selectedKeys={[entryKey]} mode="horizontal" className="qq">
+        {
+          entranceItem.map((item,key) => {
+            return (
+              <Menu.Item key={item.path}>
+                <Link to={item.path}>
+                  <SvgIcons iconClass={item.icon}/>
+                  {item.title}
+                </Link>
+              </Menu.Item>
+            )
+          })
+        }
+    </Menu>
+  )
+}
+
+const mapStateToProps = (state: any) => {
+  return {
+    permissionTabs: state.user.permissionTabs
+  }
+}
+
+export default connect(mapStateToProps)(EntryItem)
--- src/components/GlobalHeader/index.tsx
+++ src/components/GlobalHeader/index.tsx
@@ -1,43 +1,33 @@
+import React ,{useState}from 'react'
 import './GlobalHeader.scss'
 import logoPage from '@/assets/images/basic/logo.png'
-import {connect} from 'react-redux'
-import {constantsRouter} from '@/router/constants'
 import {Link } from 'react-router-dom'
-import {SvgIcons} from '@/components'
+import Entry from './components/Menu/index'
 
 
-const GlobalHeader = (props:any) => {
-  //* 入口的key 
-  const entranceKey = ['平台管理','系统管理']
-  //* 入口的内容
-  const entranceItem = constantsRouter.filter(item => entranceKey.includes(item.title))
-  const {permissionTabs} = props
+const GlobalHeader = () => {
+  const [entryKey,setEntryKey] = useState(window.location.pathname)
+
+  const goHome = () => {
+    setEntryKey('/dashboard')
+  }
+
+  const setEntryKeyOptions = (key:string) => {
+    setEntryKey(key)
+  }
+
   return (
     <div className="global-header" >
-      <Link className="global-header-info" to='/dashboard'>
+      <Link className="global-header-info" to='/dashboard' onClick={goHome}>
         <img src={logoPage} alt="" />
         <span>干部监督综合管控平台</span>
       </Link>
+
       <div className="global-header-entry">
-        {
-          entranceItem.map((item,key) => {
-            return permissionTabs.includes(item.limit) 
-              ?  <Link key={key} to={item.path} className="model-item">
-                  <SvgIcons iconClass={item.icon} />
-                  {item.title}
-                 </Link> 
-              : null
-          })
-        }
+        <Entry entryKey={entryKey} setEntryKeyOptions={setEntryKeyOptions}/>
       </div>
     </div>
   )
 }
 
-const mapStateToProps = (state: any) => {
-  return {
-    permissionTabs: state.user.permissionTabs
-  }
-}
-
-export default connect(mapStateToProps)(GlobalHeader)
+export default GlobalHeader
